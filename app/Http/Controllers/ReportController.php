d5d983d4b57f10d7b98d3ce1abc7a996f0560dea
jmearnest1s <115381541+jmearnest1s@users.noreply.github.com>
2023-11-27 20:41:49 -0600

    Create ReportController.php

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,25 @@
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Report;
+
+class ReportController extends Controller
+{
+    public function store(Request $request, Post $post)
+    {
+        // Validate the request
+        $request->validate([
+            'reason' => 'required|string',
+        ]);
+
+        // Create a new report
+        Report::create([
+            'post_id' => $post->id,
+            'user_id' => auth()->user()->id, // assuming you have user authentication
+            'reason' => $request->input('reason'),
+        ]);
+
+        return redirect()->back()->with('success', 'Post reported successfully.');
+    }
+}
